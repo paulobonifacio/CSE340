ac236b20d4796422b7de3d0e13dc599030615f88
paulobonifacio <paulo.bonifacio@live.com>
2025-03-03 19:41:06 +0100

    setup

--- server.js
+++ server.js
@@ -36,3 +36,9 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
+// Index route
+app.get("/", function(req, res) {
+  res.render("index", { title: "Home" })
+})
+
